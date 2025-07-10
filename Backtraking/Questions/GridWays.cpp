98df4b32b838ae2fbe13d887fbb98bfc57954cdf
Afzal14786 <mdafzal14777@gmail.com>
2025-07-11 00:03:48 +0530

    Implementing Logic for Solving Sudoku Problem & Grid Problem With Best Complexity

--- Backtraking/Questions/GridWays.cpp
+++ Backtraking/Questions/GridWays.cpp
@@ -7,20 +7,43 @@
  #include <vector>
  using namespace std;
 
-int Grid (int row, int col, int n, int m) {
-    if (row == n && col == m) {
+int Grid (int row, int col, int n, int m, string ans) {
+    if (row == n-1 && col == m-1) {
+        cout << ans << endl;
         return 1;
-    } else {
-        int way = Grid(row, col+1, n, m);
-        int way2 = Grid(row+1, col, n, m);
-        return way + way2;
     }
+
+    if (row >= n || col >= m) {
+        return 0;
+    }
+
+    int way = Grid(row, col+1, n, m, ans + "Right-> ");
+    int way2 = Grid(row+1, col, n, m, ans + "Down-> ");
+    return way + way2;
 }
 
- int main() {
-    int n = 4, m = 4; // Grid size (n x m)
-    int totalWays = Grid(1, 1, n-1, m-1); // Start from (1, 1) to (n, m)
-    cout << "Total ways to reach from (1, 1) to (" << n << ", " << m << ") is: " << totalWays << endl;
+// Optimize Solution //
+ 
+int fact(int n) {
+    if (n <= 1) {
+        return 1;
+    }
 
+    return n * fact(n-1);
+}
+
+int Grid2(int n, int m) {
+    int ans = fact(n-1 + m-1) / (fact(n-1) * fact(m-1));
+    return ans;
+}
+
+
+ int main() {
+    int n = 3, m = 3; // Grid size (n x m)
+    string ans = "";
+    // int ways = Grid(0, 0, n, m, ans);
+    // cout << "Total No Of Ways = " << ways << endl;
+
+    cout << Grid2(3, 3) << endl;
     return 0;
  }
