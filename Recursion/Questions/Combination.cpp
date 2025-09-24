a84912797097c8a554c30dc1017f468371b86d1c
Afzal14786 <mdafzal14777@gmail.com>
2025-09-25 03:34:52 +0530

    fixing the combination at Recursion/Questions/Combination.cpp

--- Recursion/Questions/Combination.cpp
+++ Recursion/Questions/Combination.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 // combination formula = n! / r! * (n-r)!
 
-int fact(int x) {
+unsigned long long fact(int x) {
     if (x == 1) {
         return x;
     }
@@ -11,19 +11,22 @@ int fact(int x) {
     return x * fact(x-1);
 }
 
-int Combination(int n, int r) {
-    int fact_n = fact(n);
-    int fact_r = fact(r);
-    int fact_n_r = fact(n-r);
+unsigned long long Combination(int n, int r) {
+    unsigned long long fact_n = fact(n);
+    unsigned long long fact_r = fact(r);
+    unsigned long long fact_n_r = fact(n-r);
 
     return fact_n / (fact_r * fact_n_r);
 }
 
-int Combination2(int n, int r) {
+unsigned long long Combination2(int n, int r) {
     return fact(n) / (fact(r) * fact(n-r));
 }
 
 int main() {
-    cout << "Using Combination Formula Is : " << Combination2(4,2) << endl;
+    cout << "Enter the number of objects(n) & samples(r) : ";
+    int n, r;
+    cin >> n >> r;
+    cout << "The combination is : " << Combination(n, r) << endl;
     return 0;
 }
