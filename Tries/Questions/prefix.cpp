9a8f86f61ac21b5fc22377369700550c93a036bd
Afzal14786 <mdafzal14777@gmail.com>
2025-10-18 02:33:53 +0530

    practice questions on tries like prefix count & longestWord in the dict[] with all prefixes

--- Tries/Questions/prefix.cpp
+++ Tries/Questions/prefix.cpp
@@ -27,6 +27,22 @@ class Tries {
         return count + 1;
     }
 
+    void longestHelper(Node *root, string &ans, string temp) {
+        for (pair<char, Node*> child : root->children) {
+            // only when the end of the word is true
+            if (child.second->eof) {
+                temp += child.first;
+                // check if the word is same or not also check who is greater
+                if ((temp.size() == ans.size() && temp < ans) || (temp.size() > ans.size())) {
+                    ans = temp;
+                }
+                // recursive call
+                longestHelper(child.second, ans, temp);
+                temp = temp.substr(0, temp.size() - 1);
+            }
+        }
+    }
+
     public:
         Tries() {
             root = new Node();
@@ -43,7 +59,19 @@ class Tries {
     }
 
     int uniqueSubstrings(string word);      // we are given a word and we have to find the total number of unique substrings
+    string longestWord() {
+        string ans = "";
+        longestHelper(root, ans, "");
+        return ans;
+    }
 
+    /**
+     * @brief Find the largest word in the dict[], that is available in the dist[] as well as all of it's prefixes as well
+     * @return String
+     */
+    string longestSubstring(vector<string> &dict);
+    // leetcode question no : https://leetcode.com/problems/counting-words-with-a-given-prefix/
+    int prefixCount(vector<string> &words, string prefix);
 };
 
 void Tries::insert(string key) {
@@ -139,14 +167,37 @@ void display_prefixes(vector<string> &answer) {
     cout << "}" << endl;
 }
 
+string Tries::longestSubstring(vector<string> &dict) {
+    // strp 1 : Build the tree
+    Tries trie;
+    for (int i = 0; i < dict.size(); ++i) {
+        trie.insert(dict[i]);
+    }
+
+    // step 2 : call the search 
+    return trie.longestWord();
+}
+
+int Tries::prefixCount(vector<string> &dict, string prefix) {
+    int ans = 0;
+    for (const string &word : dict) {
+        if (word.rfind(prefix, 0) == 0) ans++;
+    }
+
+    return ans;
+}
+
 int main() {
     Tries trie;
     vector<string> words = {"zebra", "dog", "duck", "dove"};
+    vector<string> dict = {"a", "banana", "app", "appl", "ap", "apply", "apple"};
     vector<string> ans  = prefix(words);
     cout << trie.startsWith("duck") << endl;
     display_prefixes(ans);
 
     // count all unique sub string that can be formed from one given string
     cout << trie.uniqueSubstrings("ababa") << endl;     // 10
+    cout << "The Longest Word With All Prefixes Exist In The Word[] : " << trie.longestSubstring(dict) << endl;
+    cout << "Prefix Count : " << trie.prefixCount(dict, "a") << endl;
     return 0;
 }
