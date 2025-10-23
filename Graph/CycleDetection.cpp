dafdef7f7a9a5b5c250a6e63bdd5e2efb86074e7
Afzal14786 <mdafzal14777@gmail.com>
2025-10-23 22:00:55 +0530

    implement cycle detection in undirected graph using dfs

---
+++ Graph/CycleDetection.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <list>
+
+using namespace std;
+
+class Graph {
+    int V;
+    vector<list<int>> listAdj;
+
+    void dfsHelper(int node, vector<bool> &visited) {
+        cout << node << " ";
+        visited[node] = true;
+
+        for (int n : listAdj[node]) {
+            if (!visited[n]) {
+                dfsHelper(n, visited);
+            }
+        }
+    }
+
+    bool cycleUndirectedHelper(int source, int parent, vector<bool> &visited) {
+        visited[source] = true;
+
+        for (int neighbour : listAdj[source]) {
+            if (!visited[neighbour]) {
+                if (cycleUndirectedHelper(neighbour, source, visited)) return true;  // cycle exist
+            } else {
+                if (neighbour != parent) return true;   // cycle exist 
+            }
+        }
+
+        return false;
+    }
+
+public:
+    Graph(int V) : V(V), listAdj() {
+        listAdj.resize(V);
+    }
+
+    // addEdge
+    void addEdge(int u, int v, bool directed = false) {
+        listAdj[u].push_back(v);
+        if (!directed) {
+            listAdj[v].push_back(u);
+        }
+    }
+
+    // print the graph
+    void printGraph() {
+        cout << "List Adjecency : \n";
+        for (int i = 0; i < V; ++i) {
+            cout << "vertex " << i << " -> ";
+            for (int neighbour : listAdj[i]) {
+                cout  << neighbour << " ";
+            }
+            cout << endl;
+        }
+    }
+
+    void dfs() {
+        vector<bool> visited(V, false);
+        dfsHelper(0, visited);
+        cout << endl;
+    }
+
+    //check the cycle exist or not using dfs
+    bool isCycleUndirected() {
+        vector<bool> visited(V, false);
+        return cycleUndirectedHelper(0, -1, visited);
+    }
+};
+
+int main() {
+    Graph graph(5);
+    graph.addEdge(0, 1);
+    // graph.addEdge(0, 2);
+    graph.addEdge(0, 3);
+    graph.addEdge(1, 2);
+    graph.addEdge(3, 4);
+
+    graph.printGraph();
+    cout << "\n---------------- DFS TRAVERSAL ----------------\n";
+    graph.dfs();
+    cout << (graph.isCycleUndirected() ? "Cycle Exist" : "Cycle Not Exist") << endl;
+    return 0;
+}
