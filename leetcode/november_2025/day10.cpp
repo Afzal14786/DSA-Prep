3d7052e3a4818aaec01f76d5b8c5fc852b71611f
Afzal14786 <mdafzal14777@gmail.com>
2025-11-10 13:47:19 +0530

    practice leetcode day10

---
+++ leetcode/november_2025/day10.cpp
@@ -0,0 +1,102 @@
+// You are given an array nums of size n, consisting of non-negative integers. Your task is to apply some (possibly zero) operations on the array so that all elements become 0.
+
+// In one operation, you can select a subarray [i, j] (where 0 <= i <= j < n) and set all occurrences of the minimum non-negative integer in that subarray to 0.
+
+// Return the minimum number of operations required to make all elements in the array 0.
+
+// Example 1:
+
+// Input: nums = [0,2]
+
+// Output: 1
+
+// Explanation:
+
+//     Select the subarray [1,1] (which is [2]), where the minimum non-negative integer is 2. Setting all occurrences of 2 to 0 results in [0,0].
+//     Thus, the minimum number of operations required is 1.
+
+// Example 2:
+
+// Input: nums = [3,1,2,1]
+
+// Output: 3
+
+// Explanation:
+
+//     Select subarray [1,3] (which is [1,2,1]), where the minimum non-negative integer is 1. Setting all occurrences of 1 to 0 results in [3,0,2,0].
+//     Select subarray [2,2] (which is [2]), where the minimum non-negative integer is 2. Setting all occurrences of 2 to 0 results in [3,0,0,0].
+//     Select subarray [0,0] (which is [3]), where the minimum non-negative integer is 3. Setting all occurrences of 3 to 0 results in [0,0,0,0].
+//     Thus, the minimum number of operations required is 3.
+
+// Example 3:
+
+// Input: nums = [1,2,1,2,1,2]
+
+// Output: 4
+
+// Explanation:
+
+//     Select subarray [0,5] (which is [1,2,1,2,1,2]), where the minimum non-negative integer is 1. Setting all occurrences of 1 to 0 results in [0,2,0,2,0,2].
+//     Select subarray [1,1] (which is [2]), where the minimum non-negative integer is 2. Setting all occurrences of 2 to 0 results in [0,0,0,2,0,2].
+//     Select subarray [3,3] (which is [2]), where the minimum non-negative integer is 2. Setting all occurrences of 2 to 0 results in [0,0,0,0,0,2].
+//     Select subarray [5,5] (which is [2]), where the minimum non-negative integer is 2. Setting all occurrences of 2 to 0 results in [0,0,0,0,0,0].
+//     Thus, the minimum number of operations required is 4.
+
+// Constraints:
+
+//     1 <= n == nums.length <= 105
+//     0 <= nums[i] <= 105
+
+// Question Link : https://leetcode.com/problems/minimum-operations-to-convert-all-elements-to-zero/?envType=daily-question&envId=2025-11-10
+// Submission Link : https://leetcode.com/problems/minimum-operations-to-convert-all-elements-to-zero/submissions/1825798751/?envType=daily-question&envId=2025-11-10
+
+#include <iostream>
+#include <vector>
+#include <set>
+#include <stack>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        vector<int> st;
+        int ops = 0;
+        
+        for (int num : nums) {
+            while (!st.empty() && st.back() > num) {
+                st.pop_back();
+            }
+            if (num == 0) continue;
+
+            if (st.empty() || st.back() < num) {
+                st.push_back(num);
+                ops++;
+            }
+        }
+        
+        return ops;
+    }
+};
+
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        stack<int> st;
+        int ops = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            while (!st.empty() && st.top() > nums[i]) st.pop();
+
+            if (nums[i] == 0) continue;
+
+            if (st.empty() || st.top() < nums[i]) {
+                st.push(nums[i]);
+                ops++;
+            }
+        }
+
+        return ops;
+    }
+};
