9c40eda8509d32b09a60b0b6fb00e305f84272a5
Afzal14786 <mdafzal14777@gmail.com>
2025-11-18 12:40:37 +0530

    practice day 18 leetcode

---
+++ leetcode/november_2025/day18.cpp
@@ -0,0 +1,63 @@
+// We have two special characters:
+
+//     The first character can be represented by one bit 0.
+//     The second character can be represented by two bits (10 or 11).
+
+// Given a binary array bits that ends with 0, return true if the last character must be a one-bit character.
+
+ 
+
+// Example 1:
+
+// Input: bits = [1,0,0]
+// Output: true
+// Explanation: The only way to decode it is two-bit character and one-bit character.
+// So the last character is one-bit character.
+
+// Example 2:
+
+// Input: bits = [1,1,1,0]
+// Output: false
+// Explanation: The only way to decode it is two-bit character and two-bit character.
+// So the last character is not one-bit character.
+
+
+// Constraints:
+
+//     1 <= bits.length <= 1000
+//     bits[i] is either 0 or 1.
+
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isOneBitCharacter(vector<int>& bits) {
+        int n = bits.size();
+        int i = 0;
+
+        while (i < n-1) {
+            i += (bits[i] == 1) ? 2 : 1;
+        }
+
+        return i == n-1;
+    }
+};
+
+class Solution {
+public:
+    bool isOneBitCharacter(vector<int>& bits) {
+        int n = bits.size();
+        int countOne = 0;
+
+        for (int i = n-2; i >= 0 && bits[i] == 1; --i) {
+            countOne++;
+        }
+
+        return countOne % 2 == 0 ? true : false;
+    }
+};
