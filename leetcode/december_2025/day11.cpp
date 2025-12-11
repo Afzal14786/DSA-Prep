54202fa13b0f34c90d908fce59c88ad6b9d253e5
Afzal14786 <mdafzal14777@gmail.com>
2025-12-12 01:24:15 +0530

    leetcode december day11

---
+++ leetcode/december_2025/day11.cpp
@@ -0,0 +1,81 @@
+// You are given a positive integer n, representing an n x n city. You are also given a 2D grid buildings, where buildings[i] = [x, y] denotes a unique building located at coordinates [x, y].
+
+// A building is covered if there is at least one building in all four directions: left, right, above, and below.
+
+// Return the number of covered buildings.
+
+// Example 1:
+
+// Input: n = 3, buildings = [[1,2],[2,2],[3,2],[2,1],[2,3]]
+// Output: 1
+// Explanation:
+
+//     Only building [2,2] is covered as it has at least one building:
+//         above ([1,2])
+//         below ([3,2])
+//         left ([2,1])
+//         right ([2,3])
+//     Thus, the count of covered buildings is 1.
+
+// Example 2:
+
+// Input: n = 3, buildings = [[1,1],[1,2],[2,1],[2,2]]
+// Output: 0
+
+// Explanation:
+
+//     No building has at least one building in all four directions.
+
+// Example 3:
+
+// Input: n = 5, buildings = [[1,3],[3,2],[3,3],[3,5],[5,3]]
+// Output: 1
+
+// Explanation:
+
+//     Only building [3,3] is covered as it has at least one building:
+//         above ([1,3])
+//         below ([5,3])
+//         left ([3,2])
+//         right ([3,5])
+//     Thus, the count of covered buildings is 1.
+
+// Constraints:
+
+//     2 <= n <= 105
+//     1 <= buildings.length <= 105 
+//     buildings[i] = [x, y]
+//     1 <= x, y <= n
+//     All coordinates of buildings are unique.
+
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <set>
+
+using namespace std;
+
+class Solution {
+public:
+    int countCoveredBuildings(int n, vector<vector<int>>& buildings) {
+        unordered_map<int, set<int>> mp_x;
+        unordered_map<int, set<int>> mp_y;
+
+        for (auto i : buildings) {
+            mp_x[i[0]].insert(i[1]);
+            mp_y[i[1]].insert(i[0]);
+        }
+
+        int count = 0;
+        for (auto i : buildings) {
+            int x = i[0];
+            int y = i[1];
+            if (*mp_x[x].begin() == y || *mp_y[y].begin() == x) continue;
+            if (*mp_x[x].rbegin() == y || *mp_y[y].rbegin() == x) continue;
+            count++;
+        }
+
+        return count++;
+    }
+};
