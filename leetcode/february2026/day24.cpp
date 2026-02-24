5ba8e29dfd9c2dcf7cc6af378531e3ef091aa467
Afzal14786 <mdafzal14777@gmail.com>
2026-02-24 12:35:32 +0530

    february: day 24

---
+++ leetcode/february2026/day24.cpp
@@ -0,0 +1,26 @@
+// Question Link : https://leetcode.com/problems/sum-of-root-to-leaf-binary-numbers/?envType=daily-question&envId=2026-02-24
+
+struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+
+class Solution {
+public:
+    int sumRootToLeaf(TreeNode* root) {
+        return dfs(root, 0);
+    }
+
+private:
+    int dfs(TreeNode* node, int curr) {
+        if (!node) return 0;
+        curr = curr * 2 + node->val; // append this bit
+        if (!node->left && !node->right) // leaf
+            return curr;
+        return dfs(node->left, curr) + dfs(node->right, curr);
+    }
+};
