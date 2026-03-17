b1bbc04b94a1f8694df146c6fc02a0b8a16ef375
Afzal14786 <mdafzal14777@gmail.com>
2026-03-17 16:23:56 +0530

    march day 17

--- leetcode/march2026/day17.cpp
+++ leetcode/march2026/day17.cpp
@@ -26,3 +26,29 @@ public:
         return res;
     }
 };
+
+
+class Solution {
+public:
+    int largestSubmatrix(vector<vector<int>>& matrix) {
+        int n = matrix.size() ;
+        int m = matrix[0].size() ;
+         int maxarea = 0 ;  
+        for(int i = 0 ; i<n ;i++){
+            for(int j = 0 ; j<m ;j++){
+                if(matrix[i][j] == 1 && i-1>=0){
+                    matrix[i][j] += matrix[i-1][j];
+                }
+            }
+            vector<int>heights = matrix[i] ;
+            sort(heights.begin() , heights.end() , greater<int>()) ;
+            for(int k =0 ; k<m ;k++){
+                int ht = heights[k] ;
+                int base = k+1 ;
+                maxarea = max(maxarea , ht*base) ;
+
+            }
+        }
+        return maxarea ;
+    }
+};
