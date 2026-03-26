e7d5b07f6431087745924e98a687d65e2abdcfae
Afzal14786 <mdafzal14786@gmail.com>
2026-03-26 13:17:50 +0530

    march day26

---
+++ leetcode/march2026/day26.cpp
@@ -0,0 +1,76 @@
+// Question Link : https://leetcode.com/problems/equal-sum-grid-partition-ii/?envType=daily-question&envId=2026-03-26
+
+#include <iostream>
+#include <unordered_set>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> rotation(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        vector tmp(n, vector<int>(m));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                tmp[j][m - 1 - i] = grid[i][j];
+            }
+        }
+        return tmp;
+    }
+    bool canPartitionGrid(vector<vector<int>>& grid) {
+        long long total = 0;
+        long long sum;
+        long long tag;
+        int m = grid.size();
+        int n = grid[0].size();
+        unordered_set<long long> exist;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                total += grid[i][j];
+            }
+        }
+        for (int k = 0; k < 4; k++) {
+            exist.clear();
+            exist.insert(0);
+            sum = 0;
+            m = grid.size();
+            n = grid[0].size();
+            if (m < 2) {
+                grid = rotation(grid);
+                continue;
+            }
+            if (n == 1) {
+                for (int i = 0; i < m - 1; i++) {
+                    sum += grid[i][0];
+                    tag = sum * 2 - total;
+                    if (tag == 0 || tag == grid[0][0] || tag == grid[i][0]) {
+                        return true;
+                    }
+                }
+                grid = rotation(grid);
+                continue;
+            }
+            for (int i = 0; i < m - 1; i++) {
+                for (int j = 0; j < n; j++) {
+                    exist.insert(grid[i][j]);
+                    sum += grid[i][j];
+                }
+                tag = sum * 2 - total;
+                if (i == 0) {
+                    if (tag == 0 || tag == grid[0][0] ||
+                        tag == grid[0][n - 1]) {
+                        return true;
+                    }
+                    continue;
+                }
+                if (exist.contains(tag)) {
+                    return true;
+                }
+            }
+            grid = rotation(grid);
+        }
+        return false;
+    }
+};
