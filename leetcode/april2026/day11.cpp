395b75f48b7545071b81a12355acf44eef8f3275
Afzal14786 <mdafzal14777@gmail.com>
2026-04-11 10:32:37 +0530

    April(leetcode): Day11 ~ minimum-distance-between-three-equal-elements-ii

---
+++ leetcode/april2026/day11.cpp
@@ -0,0 +1,52 @@
+// Question Link : https://leetcode.com/problems/minimum-distance-between-three-equal-elements-ii/description/?envType=daily-question&envId=2026-04-11
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    int minimumDistance(std::vector<int>& nums) {
+        int ans = 1e9;
+        int n = nums.size();
+        for (int i = 0; i < n; ++i) {
+            bool flag = false;
+            for (int j = i+1; j < n; ++j) {
+                if (nums[i] == nums[j]) {
+                    if (flag) {
+                        ans = std::min(ans, j-i);
+                        break;
+                    } else {
+                        flag = true;
+                    }
+                }
+            }
+        }
+
+        return (ans == 1e9) ? -1 : ans * 2;
+    }
+};
+
+/**
+ * Time Compexity : O(N)
+ * Space Compexity: O(N) In order to storing the data for manipulating
+ */
+class Solution {
+public:
+    int minimumDistance(std::vector<int>& nums) {
+        int ans = 1e9;
+        int n = nums.size();
+        std::vector<int> position(n+1, -1);
+        std::vector<int> distance(n, 1e9);
+
+        for (int i = 0; i < n; ++i) {
+            if (position[nums[i]] != -1) {
+                distance[i] = (i - position[nums[i]]);
+                ans = std::min(ans, distance[i] + distance[position[nums[i]]]);
+            }
+            position[nums[i]] = i;
+        }
+
+        return (ans == 1e9) ? -1 : 2 * ans;
+    }
+};
