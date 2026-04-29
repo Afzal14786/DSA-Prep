822b8ded3a34231399f1819097b5a61c7a6ce785
Afzal14786 <mdafzal14777@gmail.com>
2026-04-29 14:27:32 +0530

    Practice code for coin change with another code

--- dp/knapsack/unbound-knapsack/coinChange.cpp
+++ dp/knapsack/unbound-knapsack/coinChange.cpp
@@ -84,6 +84,37 @@ public:
     }
 };
 
+/**
+ * Another solution using 2D
+ */
+
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        int INF = 1e9; 
+        vector<vector<int>> dp(n + 1, vector<int>(amount + 1, 0));
+        // If amount is 0, we need 0 coins
+        for (int i = 0; i <= n; i++) dp[i][0] = 0;
+
+        // If we have 0 coins, we can't make any amount > 0
+        for (int j = 1; j <= amount; ++j) dp[0][j] = INF;
+
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= amount; ++j) {
+                if (coins[i-1] <= j) {
+                    // Min of excluding the coin vs. including it (1 + same row)
+                    dp[i][j] = min(dp[i-1][j], 1 + dp[i][j-coins[i-1]]);
+                } else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+
+        return (dp[n][amount] >= INF) ? -1 : dp[n][amount];
+    }
+};
+
 int coinChange(int amount, vector<int> coins) {
     int n = coins.size();
 
