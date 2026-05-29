4f057edafe871a6c20420b7b9a4b82e2ecdc04bc
Afzal14786 <mdafzal14777@gmail.com>
2026-05-29 15:11:14 +0530

    May 2026 : Day 29 ~ minimum-element-after-replacement-with-digit-sum

---
+++ leetcode/may2026/day29.cpp
@@ -0,0 +1,17 @@
+// Question Link : https://leetcode.com/problems/minimum-element-after-replacement-with-digit-sum/?envType=daily-question&envId=2026-05-29
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int minElement(vector<int>& nums) {
+        int res = 36;
+        for (auto& n : nums)
+            res = min(res, n - 9 * ((n/10) + (n/100) + (n/1000) + (n/10000)));
+        
+        return res;
+    }
+};
