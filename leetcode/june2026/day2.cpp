4ee82f942f131cd32feb5c4666a3c9582c4b9a4f
Afzal14786 <mdafzal14777@gmail.com>
2026-06-02 10:04:25 +0530

    June 2026 Day 2 : earliest-finish-time-for-land-and-water-rides-i

---
+++ leetcode/june2026/day2.cpp
@@ -0,0 +1,29 @@
+// Question Link : https://leetcode.com/problems/earliest-finish-time-for-land-and-water-rides-i/description/?envType=daily-question&envId=2026-06-02
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int calculateFinishTime(vector<int>& landStartTime, vector<int>& landDuration, vector<int>& waterStartTime, vector<int>& waterDuration) {
+        int minimum = 1e9;
+
+        for (int i = 0; i < landStartTime.size(); ++i) {
+            minimum = min(minimum, landStartTime[i] + landDuration[i]);
+        }
+
+        int ans = 1e9;
+
+        for (int i = 0; i < waterStartTime.size(); ++i) {
+            ans = min(ans, max(minimum, waterStartTime[i]) + waterDuration[i]);
+        }
+
+        return ans;
+    }
+
+    int earliestFinishTime(vector<int>& landStartTime, vector<int>& landDuration, vector<int>& waterStartTime, vector<int>& waterDuration) {
+        return min(calculateFinishTime(landStartTime, landDuration, waterStartTime, waterDuration), calculateFinishTime(waterStartTime, waterDuration, landStartTime, landDuration));
+    }
+};
