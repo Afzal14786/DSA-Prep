b49ea1b90e28f90c6fd10232ab100ed47d8213c0
Afzal14786 <mdafzal14777@gmail.com>
2026-07-24 16:40:31 +0530

    Optimize space complexity

--- leetcode/july2026/day24.cpp
+++ leetcode/july2026/day24.cpp
@@ -29,6 +29,41 @@ public:
     }
 };
 
+class Solution {
+public:
+    int uniqueXorTriplets(vector<int>& nums) {
+        int n = nums.size();
+        int max_elem = *max_element(nums.begin(), nums.end());
+
+        int T = 1;
+        while (T <= max_elem)  T *= 2;
+
+        vector<bool> S1(T, false);  // XOR for pairs
+        vector<bool> s2(T, false); // XOR for triplates
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = i; j < n; ++j) {
+                S1[nums[i] ^ nums[j]] = true;
+            }
+        }
+
+        for (int i = 0; i < T; ++i) {
+            if (S1[i]) {
+                for (int num : nums) {
+                    S2[i ^ num] = true;
+                }
+            }
+        }
+
+        int count = 0;
+        for (bool x : S2) {
+            if (x) count++;
+        }
+
+        return count;
+    }
+};
+
 int main() {
     
     return 0;
