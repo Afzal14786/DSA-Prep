2a16dfa59d719271dc63bf7f8a30a5882b46791b
Afzal14786 <mdafzal14777@gmail.com>
2026-08-03 18:35:40 +0530

    Improve the solution using constant space

--- leetcode/august2026/day3.cpp
+++ leetcode/august2026/day3.cpp
@@ -62,3 +62,29 @@ public:
         return "Tie";
     }
 };
+
+// with constant space 
+class Solution {
+public:
+    string stoneGameIII(vector<int>& stones) {
+        int n = stones.size();
+        int a = 0, b = 0, c = 0;
+
+        for (int i = n - 1; i >= 0; --i) {
+            int res = INT_MIN;
+            res = max(res, stones[i] - a);
+            if (i + 2 <= n)
+                res = max(res, stones[i] + stones[i+1] - b);
+            if (i + 3 <= n)
+                res = max(res, stones[i] + stones[i+1] + stones[i+2] - c);
+            
+            c = b;
+            b = a;
+            a = res;
+        }
+
+        if (a < 0) return "Bob";
+        else if (a > 0) return "Alice";
+        return "Tie";
+    }
+};
