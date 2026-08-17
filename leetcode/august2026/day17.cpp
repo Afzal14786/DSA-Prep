450da099a80e23be383bab08e8b22327937a3f26
Afzal14786 <mdafzal14777@gmail.com>
2026-08-17 12:04:38 +0530

    August Day 17 stone-game-v -- Memoization

--- leetcode/august2026/day17.cpp
+++ leetcode/august2026/day17.cpp
@@ -9,6 +9,40 @@ using namespace std;
 
 class Solution {
 public:
+    int stoneGameV(vector<int>& stone_value) {
+        int n = stone_value.size();
+        vector<int> prefix_sum(n, 0);
+        prefix_sum[0] = stone_value[0];
+        for (int i = 1; i < n; ++i) prefix_sum[i] = prefix_sum[i-1] + stone_value[i];
+
+        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+
+        for (int l = n-1; l >= 0; --l) {
+            for (int r = l+1; r < n; ++r) {
+                int score = 0;
+                for (int mid = l; mid <= r - 1; ++mid) {
+                    int left_sum  = prefix_sum[mid] - (l - 1 >= 0 ? prefix_sum[l-1] : 0);
+                    int right_sum = prefix_sum[r] - prefix_sum[mid];
+
+                    if (left_sum < right_sum) {
+                        score = max(score, left_sum + dp[l][mid]);
+                    } else if (left_sum > right_sum) {
+                        score = max(score, right_sum + dp[mid + 1][r]);
+                    } else {
+                        score = max({score, left_sum + dp[l][mid], right_sum + dp[mid+1][r]});
+                    }
+                }
+
+                dp[l][r] = score;
+            }
+        }
+
+        return dp[0][n-1];
+    }
+};
+
+class Solution {
+public:
     int memo[501][501];
     int solve(int l, int r, vector<int> &prefix_sum) {
         if (l >= r) return 0;
