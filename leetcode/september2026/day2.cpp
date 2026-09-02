f116babb09b9e6f8dac045e0aaae27f6dad74272
Afzal14786 <mdafzal14777@gmail.com>
2026-09-02 09:27:03 +0530

    September Day 2 construct-uniform-parity-array-i

---
+++ leetcode/september2026/day2.cpp
@@ -0,0 +1,14 @@
+
+// رَبِّ زِدْنِي عِلْمًا
+// اے میرے رب! میرے علم میں اضافہ فرما۔
+#include <bits/stdc++.h>
+using namespace std;
+
+// Question Link : https://leetcode.com/problems/construct-uniform-parity-array-i/description/?envType=daily-question&envId=2026-09-02
+
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        return true;
+    }
+};
