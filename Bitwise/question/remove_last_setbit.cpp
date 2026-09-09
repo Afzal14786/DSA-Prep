87229bce3e83522d8d1410ff7ea60f3dc98755a3
Afzal14786 <mdafzal14777@gmail.com>
2026-09-09 12:41:15 +0530

    removing the last setbit using bits

---
+++ Bitwise/question/remove_last_setbit.cpp
@@ -0,0 +1,13 @@
+// رَبِّ زِدْنِي عِلْمًا
+// اے میرے رب! میرے علم میں اضافہ فرما۔
+#include <bits/stdc++.h>
+using namespace std;
+
+void remove_last_setbit(int N) {
+    ((N & N-1));
+}
+
+int main() {
+    
+    return 0;
+}
