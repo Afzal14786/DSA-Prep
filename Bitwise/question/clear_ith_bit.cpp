7d6f3e38324ab61b3d7c7ddf2d0d5d36db8e1123
Afzal14786 <mdafzal14777@gmail.com>
2026-09-09 12:39:27 +0530

    clear set-bit

---
+++ Bitwise/question/clear_ith_bit.cpp
@@ -0,0 +1,13 @@
+// رَبِّ زِدْنِي عِلْمًا
+// اے میرے رب! میرے علم میں اضافہ فرما۔
+#include <bits/stdc++.h>
+using namespace std;
+
+void clear_bit(int N, int i) {
+    (N & ~(1 << i));
+}
+
+int main() {
+    
+    return 0;
+}
