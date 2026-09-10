bca14db258bf03135b64c5aa1f77d5a456b0b9c6
Afzal14786 <mdafzal14777@gmail.com>
2026-09-10 11:08:15 +0530

    single number ii efficient solution

--- Bitwise/question/single_number_ii.cpp
+++ Bitwise/question/single_number_ii.cpp
@@ -3,6 +3,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/**
+ * more optmize solution
+ */
+
+class Solution {
+ public:
+  int singleNumber(vector<int>& nums) {
+    int ones = 0;
+    int twos = 0;
+
+    for (const int num : nums) {
+      ones ^= (num & ~twos);
+      twos ^= (num & ~ones);
+    }
+
+    return ones;
+  }
+};
+
+/**
+ * efficient solution
+ */
+
+ class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n = nums.size();
+        int ans = 0;
+        for (int bit_idx = 0; bit_idx <= 31; ++bit_idx) {
+            int count = 0;
+            for (int i = 0; i <= n-1; ++i) {
+                if (nums[i] & (1 << bit_idx)) count++;
+            }
+
+            if (count % 3 == 1) ans |= (1 << bit_idx);
+        }
+
+        return ans;
+    }
+};
+
 // brute force using hash map
 class Solution {
 public:
