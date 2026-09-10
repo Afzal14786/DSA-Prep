c4fa4a85311b64909f1a68a09bb2d6c48b61b2e2
Afzal14786 <mdafzal14777@gmail.com>
2026-09-10 11:20:53 +0530

    single number ii using sorting

--- Bitwise/question/single_number_ii.cpp
+++ Bitwise/question/single_number_ii.cpp
@@ -44,6 +44,20 @@ public:
     }
 };
 
+// another solution using sorting 
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        for (int i = 1; i < n; i += 3) {
+            if (nums[i] != nums[i-1]) return nums[i-1];
+        }
+        return nums[n-1];
+    }
+};
+
 // brute force using hash map
 class Solution {
 public:
