f4288162ae0cdfe599119ff88e25aef62d03df86
Afzal14786 <mdafzal14777@gmail.com>
2026-09-14 17:33:41 +0530

    minimum-flips-to-make-a-or-b-equal-to-c using approach 2 and fix approach 1's solution

--- Bitwise/question/minimum-flips-to-make-a-or-b-equal-to-c.cpp
+++ Bitwise/question/minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -9,7 +9,7 @@ public:
     int minFlips(int a, int b, int c) {
         int flips = 0;
         while (a != 0 || b != 0 || c != 0) {
-            if ((c & 1) == 0) {
+            if ((c & 1) == 1) {
                 // get the right most bit
                 if ((a & 1) == 0 && (b & 1) == 0) flips++;
             } else {
@@ -18,11 +18,31 @@ public:
 
                 // flips += ((a & 1) == 1) + ((b & 1) == 1);
             }
-            a >> 1;
-            b >> 1;
-            c >> 1;
+            a >>= 1;
+            b >>= 1;
+            c >>= 1;
         }
 
         return flips;
+    }
+};
+
+// another approach 
+
+int count_setbit(int n) {
+    int count = 0;
+    while (n != 0) {
+        n = n & (n-1);
+        count++;
+    }
+
+    return count;
+}
+
+class Solution {
+public:
+    int minFlips(int a, int b, int c) {
+        int res = ((a | b) ^ c);
+        return __builtin_popcount(res) + __builtin_popcount((a & b) & res);
     }
 };
