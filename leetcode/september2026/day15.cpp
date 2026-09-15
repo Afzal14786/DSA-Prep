535c482f17095395be7bf24df18c7861ca943369
Afzal14786 <mdafzal14777@gmail.com>
2026-09-16 01:01:41 +0530

    September Day 15 maximum-number-of-non-overlapping-palindrome-substrings

---
+++ leetcode/september2026/day15.cpp
@@ -0,0 +1,31 @@
+// رَبِّ زِدْنِي عِلْمًا
+// اے میرے رب! میرے علم میں اضافہ فرما۔
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxPalindromes(string s, int k) {
+        int n = s.size();
+        if (k == 1) return n;
+
+        int res = 0;
+
+        for (int i = 0; i <= n - k;) {
+            int step = 1;
+
+            for (int d : {k, k + 1}) {
+                if (i + d <= n && equal(s.begin() + i, s.begin() + i + (d >> 1),
+                                        s.rbegin() + (n - (i + d)))) {
+                    step = d;
+                    res++;
+                    break;
+                }
+            }
+
+            i += step;
+        }
+
+        return res;
+    }
+};
