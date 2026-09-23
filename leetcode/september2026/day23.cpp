2d7086f78282e20846bbaf83f5679bd00a34f217
Afzal14786 <mdafzal14777@gmail.com>
2026-09-23 21:39:40 +0530

    September Day 23 minimum-operations-to-reduce-x-to-zero

---
+++ leetcode/september2026/day23.cpp
@@ -0,0 +1,34 @@
+// رَبِّ زِدْنِي عِلْمًا
+// اے میرے رب! میرے علم میں اضافہ فرما۔
+#include <bits/stdc++.h>
+using namespace std;
+
+// Question Link : https://leetcode.com/problems/minimum-operations-to-reduce-x-to-zero/?envType=daily-question&envId=2026-09-23
+
+
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int x) {        
+        int n    = nums.size();
+        int sum  = 0;
+        unordered_map<int, int> mp;
+        mp[0] = -1;
+        for(int i = 0; i<n; i++) {
+            sum += nums[i];
+            mp[sum] = i;
+        }
+        if(sum < x)
+            return -1;
+        int restSum  = sum-x;
+        int longest  = INT_MIN;
+        sum = 0;
+        for(int i = 0; i<n; i++) {
+            sum += nums[i];
+            if(mp.count(sum-restSum)) {
+                longest = max(longest, i-mp[sum-restSum]);
+            }
+        }
+        
+        return longest==INT_MIN?-1:n-longest;
+    }
+};
